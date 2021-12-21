cf01b8ef75cb550481b94dbd45f5e0085455e74d
신승혁 <Cozak92@gmail.com>
2021-12-21 23:09:53 +0900

    initial commit

---
+++ src/main/kotlin/com/web/devvy/DevvyApplication.kt
@@ -0,0 +1,11 @@
+package com.web.devvy
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class DevvyApplication
+
+fun main(args: Array<String>) {
+	runApplication<DevvyApplication>(*args)
+}
---
+++ src/test/kotlin/com/web/devvy/DevvyApplicationTests.kt
@@ -0,0 +1,13 @@
+package com.web.devvy
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class DevvyApplicationTests {
+
+	@Test
+	fun contextLoads() {
+	}
+
+}

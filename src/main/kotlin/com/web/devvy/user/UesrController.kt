2bd55fea9669750ad29e7db10334b761b6628436
cozak <cozak92@gmail.com>
2021-12-23 15:09:53 +0900

    🔨 유저 컨트롤러 & 유저 컨트롤러 파일 생성

---
+++ src/main/kotlin/com/web/devvy/user/UesrController.kt
@@ -0,0 +1,28 @@
+package com.web.devvy.user
+
+import org.springframework.stereotype.Controller
+import org.springframework.web.bind.annotation.GetMapping
+import org.springframework.web.bind.annotation.PostMapping
+import org.springframework.web.bind.annotation.ResponseBody
+
+@Controller
+class UesrController {
+
+    @GetMapping("/user")
+    @ResponseBody
+    fun getUsersList(): Array<String>{
+        val user = arrayOf("신승혁,30", "최현민,30")
+        return user
+    }
+
+    @GetMapping("/user/{id}")
+    @ResponseBody
+    fun getOneUser(id: Int):String{
+        return "신승혁,30"
+    }
+
+    @PostMapping("/user")
+    fun registerOneUser(){
+
+    }
+}
---
+++ src/test/kotlin/com/web/devvy/user/controller/UserControllerTest.kt
@@ -0,0 +1,4 @@
+package com.web.devvy.user.controller
+
+class UserControllerTest {
+}

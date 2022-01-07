dc23cdccfe69ca8e5ebfe8ae86886d18cadde11b
신승혁 <Cozak92@gmail.com>
2022-01-07 20:06:42 +0900

    ✨ 프로필컨트롤러 생성

---
+++ src/main/kotlin/com/web/devvy/WebRestController.kt
@@ -0,0 +1,22 @@
+package com.web.devvy
+
+import lombok.RequiredArgsConstructor
+import org.springframework.beans.factory.annotation.Autowired
+import org.springframework.core.env.Environment
+import org.springframework.web.bind.annotation.GetMapping
+import org.springframework.web.bind.annotation.RestController
+import java.util.*
+
+
+@RestController
+@RequiredArgsConstructor
+class WebRestController {
+
+    @Autowired
+    private lateinit var env: Environment
+
+    @GetMapping("/profile")
+    fun getProfile(): String{
+        return Arrays.stream(env.activeProfiles).findFirst().orElse("");
+    }
+}

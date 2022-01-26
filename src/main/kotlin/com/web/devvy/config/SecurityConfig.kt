8bfa30e0ff102edd9f164df7fd80d04c0e656d15
신승혁 <Cozak92@gmail.com>
2022-01-26 21:02:18 +0900

    ✨ SecurityConfig.kt.kt 생성

---
+++ src/main/kotlin/com/web/devvy/config/SecurityConfig.kt
@@ -0,0 +1,69 @@
+package com.web.devvy.config
+
+import com.web.devvy.jwt.JwtAccessDeniedHandler
+import com.web.devvy.jwt.JwtAuthenticationEntryPoint
+import com.web.devvy.jwt.JwtSecurityConfig
+import com.web.devvy.jwt.TokenProvider
+import org.springframework.context.annotation.Bean
+import org.springframework.security.config.annotation.method.configuration.EnableGlobalMethodSecurity
+import org.springframework.security.config.annotation.web.builders.HttpSecurity
+import org.springframework.security.config.annotation.web.builders.WebSecurity
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter
+import org.springframework.security.config.http.SessionCreationPolicy
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder
+import org.springframework.security.crypto.password.PasswordEncoder
+import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter
+import org.springframework.web.filter.CorsFilter
+
+
+@EnableWebSecurity
+@EnableGlobalMethodSecurity(prePostEnabled = true)
+class SecurityConfig(
+    private val tokenProvider: TokenProvider,
+    private val corsFilter: CorsFilter,
+    private val jwtAuthenticationEntryPoint: JwtAuthenticationEntryPoint,
+    private val jwtAccessDeniedHandler: JwtAccessDeniedHandler
+) : WebSecurityConfigurerAdapter() {
+    @Bean
+    fun passwordEncoder(): PasswordEncoder {
+        return BCryptPasswordEncoder()
+    }
+
+    override fun configure(web: WebSecurity) {
+        web.ignoring()
+            .antMatchers(
+                "/error"
+            )
+    }
+
+    @Throws(Exception::class)
+    override fun configure(httpSecurity: HttpSecurity) {
+        httpSecurity
+            // token을 사용하는 방식이기 때문에 csrf를 disable합니다.
+            .csrf().disable()
+            .addFilterBefore(corsFilter, UsernamePasswordAuthenticationFilter::class.java)
+            .exceptionHandling()
+            .authenticationEntryPoint(jwtAuthenticationEntryPoint)
+            .accessDeniedHandler(jwtAccessDeniedHandler)
+
+            // enable h2-console
+            .and()
+            .headers()
+            .frameOptions()
+            .sameOrigin()
+
+            // 세션을 사용하지 않기 때문에 STATELESS로 설정
+            .and()
+            .sessionManagement()
+            .sessionCreationPolicy(SessionCreationPolicy.STATELESS)
+
+            .and()
+            .authorizeRequests()
+            .antMatchers("/user").permitAll()
+            .anyRequest().authenticated()
+
+            .and()
+            .apply(JwtSecurityConfig(tokenProvider))
+    }
+}
